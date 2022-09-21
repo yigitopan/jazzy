8dadb89cd22aca942135ddb2ce7b3597a510ce7f
yigitopan <yigitopan@hotmail.com>
2022-09-22 00:37:24 +0300

    blog bekleniyor

---
+++ js/paramToProduct.js
@@ -0,0 +1,64 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
+
+
+const getProductDetails = () => {
+    const urun = getUrlParameter('urun');
+    const attributes = urun.split("-");
+    
+    let tad;
+    let tur;
+    let gr;
+    let title;
+
+    if(attributes[0] == "kedi") {
+        tur = "Kedi";
+    }
+    else {
+        tur = "Köpek";
+    }
+    /////////////////////////////////////////////
+    if(attributes[1] == "ton") {
+        tad = "Ton";
+        title = "TON BALIKLI JAZZY";
+    }
+    else if (attributes[1] == "tavuk") {
+        tad = "Tavuk";
+        title = "TAVUK ETLİ JAZZY";
+    }
+    else {
+        tad = "Sığır";
+        title = "SIĞIR ETLİ JAZZY";
+    }
+    /////////////////////////////////////////////
+    if(attributes[2] == "400gr") {
+        gr = "400 Gr";
+    }
+    else {
+        tur = "95 Gr";
+    }
+    document.querySelector(".type-dynamic").innerHTML = tur;
+    document.querySelector(".taste-dynamic").innerHTML = tad;
+    document.querySelector(".gr-dynamic").innerHTML = gr;
+    document.querySelector(".product-info h2").innerHTML = title;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var urun = getUrlParameter('urun');
+    document.querySelector(".product-main-img").setAttribute("src",`assets/png-products/${urun}.png`)
+    
+    getProductDetails();
+}, false);
